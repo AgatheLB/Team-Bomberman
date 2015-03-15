b46cc47c8f43c840285c9469a75e6ddb55544ff7
Agathe Le Bouler <agathe.lebouler@gmail.com>
2015-03-15 16:03:01 +0100

    Blocs
    Ajout de la méthode d'initialisation des positions des blocs dans la
    Grid ! yey ^^

--- Matrix.py
+++ Matrix.py
@@ -1,19 +1,28 @@
 ﻿import pygame
 import numpy
 import random
+import constantes
 
 class Matrix:
     def __init__(self):
-        self.grid=creatematrice(10,10)
-    def creatematrice(ligne,colonne):
+        self.grid=self.creatematrice(constantes.CONST_NbLignes,constantes.CONST_NbColonnes)
+    def creatematrice(self,ligne,colonne):
      return [[0 for q in range(0,ligne)] for p in range(0,colonne)]
     def getvalue(self,ligne,colonne):
      return self.grid[ligne][colonne]
     def setvalue (self,ligne,colonne,value):
         self.grid[ligne][colonne]=value
     def createrandommatrice (self):
-        for i in range(10):
-
+        for ligne in range (constantes.CONST_NbLignes):
+            ListCompleted=True
+            ListColonnes=[]
+            while ListCompleted:
+                colonne=random.randint(0,constantes.CONST_NbColonnes-1)
+                if not colonne in ListColonnes :
+                    ListColonnes.append(colonne)
+                    self.setvalue(ligne,colonne,constantes.CONST_BlocValue)
+                if len(ListColonnes)==3 :
+                    ListCompleted=False
 
 if __name__ == "__main__":
     M1=Matrix()
@@ -22,3 +31,5 @@ if __name__ == "__main__":
     print M1.getvalue(1,2)
     M1.setvalue(1,2,1)
     print M1.getvalue(1,2)
+    M1.createrandommatrice()
+    print M1.grid
---
+++ constantes.py
@@ -0,0 +1,7 @@
+﻿#Valeurs du dimensionnement de la grille
+CONST_NbLignes=10
+CONST_NbColonnes=10
+CONST_NbBlocsParLigne=3
+
+# Valeurs du contenu des cases
+CONST_BlocValue=1
