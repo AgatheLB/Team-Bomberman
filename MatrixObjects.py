bff9c2a2fe760154f1f77cdb4fa681b5666f2791
Agathe Le Bouler <agathe.lebouler@gmail.com>
2015-03-21 19:06:46 +0100

    Quelques avancements
    Débuggage de la page d'accueil de Céleste ;)
    Création position joueur 2 dans Matrix
    avancement de la classe joueur dans Man

---
+++ MatrixObjects.py
@@ -0,0 +1,12 @@
+﻿import pygame
+
+class MatrixObjects:
+    def __init__(self,ligne,colonne):
+        self.x=ligne
+        self.y=colonne
+
+
+if __name__ == "__main__":
+    M1=MatrixObjects(1,2)
+    print M1.x
+    print M1.y
--- constantes.py
+++ constantes.py
@@ -1,7 +1,11 @@
 ﻿#Valeurs du dimensionnement de la grille
-CONST_NbLignes=10
-CONST_NbColonnes=10
+CONST_NbLignes=10+2
+CONST_NbColonnes=10+2
 CONST_NbBlocsParLigne=3
 
-# Valeurs du contenu des cases
-CONST_BlocValue=1
+
+#Valeurs du contenu des cases
+CONST_VideValue=0
+CONST_Bloc=1
+CONST_Perso1=8
+CONST_Perso2=9
